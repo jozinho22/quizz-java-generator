079285e7d03ea782be7dd37294368ea9f7a5f46b
jozinho22 <josselin.douineau.1987@gmail.com>
2020-05-21 23:32:30 +0200

    projet OK - plus qu'à implémenter

--- src/main/java/com/douineau/main/JsonFileGenerator.java
+++ src/main/java/com/douineau/main/JsonFileGenerator.java
@@ -1,4 +1,4 @@
-package main;
+package com.douineau.main;
 
 import java.io.IOException;
 import java.util.ArrayList;
--- src/main/java/com/douineau/main/QuestionsInsertor.java
+++ src/main/java/com/douineau/main/QuestionsInsertor.java
@@ -1,30 +1,36 @@
-package main;
+package com.douineau.main;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
+import com.douineau.dao.QuestionDao;
 import com.douineau.entity.Question;
 import com.douineau.utils.FileReader;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class JsonFileReader {
+public class QuestionsInsertor {
 
 	public static void main(String[] args) throws IOException {
+		
 		FileReader reader = new FileReader();
 		File jsonFile = reader.getFile("questions/datas.json");
-//		String json = jsonFile.
 		System.out.println(jsonFile);
 
 		ObjectMapper mapper = new ObjectMapper();
+
 //		List<Question> questions = (List<Question>) mapper.readValue(json, Question.class);
 		
 		List<Question> questions = mapper.reader()
 			      .forType(new TypeReference<List<Question>>() {})
 			      .readValue(jsonFile);
-		
-		System.out.println(questions);
+				
+		for(Question question : questions) {
+			question.setCreatedAt(new Date());
+			QuestionDao.insertQuestion(question);
+		}
 
 	}
 
