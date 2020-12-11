ef7fcbdae32895dea8cc834983ae766e028dfe63
jozinho22 <josselin.douineau.1987@gmail.com>
2020-12-11 18:55:15 +0100

    ajout de getName pour accéder depuis tous les projets

--- src/main/java/com/douineau/utils/TopicEnum.java
+++ src/main/java/com/douineau/utils/TopicEnum.java
@@ -4,10 +4,14 @@ public enum TopicEnum {
 
 	JAVA("Java"), SPRING("Spring"), JPA("JPA"), GIT("Git"), MAVEN("Maven"), DESIGN_PATTERNS("Design Patterns"), FRAMEWORKS("Frameworks de Java JEE"), SQL("SQL"), DIVERS("Divers"), ALGO("Algorithmie");
 	
-	private String topic;
+	private String name;
 	
-	TopicEnum(String topic) {
-		this.topic = topic;
+	TopicEnum(String name) {
+		this.name = name;
 	}
+	
+	public String getName() {
+		return name;
+	}	
 		
 }
